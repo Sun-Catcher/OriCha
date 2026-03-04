bd8143a942cf2f00007f957125d5a6e7c3c92d28
Felipe Robert Rodrigues de Oliveira <felipitobolito@gmail.com>
2026-03-03 22:25:23 -0300

    Quartz sync: Mar 3, 2026, 10:25 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "the.white.cat",
+    baseUrl: "the-white-cat",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
